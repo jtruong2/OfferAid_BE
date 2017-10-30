131ae32c88ec815eef850eeece21925cea5aefb1
jtruong2 <jtruong2@comcast.net>
2017-10-30 15:19:33 -0600

    modify csrf to accept json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery unless:-> {request.format.json?}
 
   def log_in(user)
     session[:user_id] = user.id
   end
-  
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
