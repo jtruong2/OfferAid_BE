1e6a495075ab6e1901efb10e82a7f4500d6d6701
jtruong2 <jtruong2@comcast.net>
2017-11-06 10:26:04 -0700

    render donation items instead of items

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ItemsController < ApplicationController
   def index
     donation = Donation.find(params[:donation_id])
-    render json: donation.items
+    render json: donation.donation_items
   end
 
   def show
