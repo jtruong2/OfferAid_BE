e0efebd8f75e5352d19158d625e5a2d968d9008f
jtruong2 <jtruong2@comcast.net>
2017-11-20 09:43:06 -0700

    alexa VUI can create a donation

--- app/controllers/api/v1/alexa_donations_controller.rb
+++ app/controllers/api/v1/alexa_donations_controller.rb
@@ -1,11 +1,17 @@
 class Api::V1::AlexaDonationsController < ApplicationController
   def create
-    byebug
+    items = Item.convert_from_voice(safe_params[:items])
+    donation = Donation.create!(
+      user_id: safe_params[:user_id],
+      pickup_date: safe_params[:pickup_date],
+      pickup_address: safe_params[:pickup_address]
+    )
+    a = DonationItem.create_association(donation.id, items)
   end
 
   private
 
   def safe_params
-    params.permit(:items, :pickup_date, :pickup_address)
+    params.permit(:items, :pickup_date, :pickup_address, :user_id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,17 @@ class Item < ApplicationRecord
     item_names = items.map { |item| item.item }
     where(name: item_names)
   end
+
+  def self.convert_from_voice(items)
+    items_as_obj = []
+    items_with_quantity = items.split(' ').each_slice(2).to_a
+    items_with_quantity.map do |item|
+      item_obj = {}
+      item_obj['id'] = Item.find_by(name: item[1]).id
+      item_obj['quantity'] = item[0].to_i
+      item_obj['name'] = item[1]
+      items_as_obj.push(item_obj)
+    end
+    return items_as_obj
+  end
 end
